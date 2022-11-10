6ef2a56e420c3b0cac7d62af22d28b5da7d87b60
Vaso <vasothom@hotmail.com>
2022-11-10 09:42:23 +0100

    user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer } from "react";
+import user from "./user";
+
+const initState = {
+  inputEmail: "",
+  inputPassword: "",
+  loggedInUser: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "updateInputEmail":
+      state = {
+        ...state,
+        inputEmail: action.value,
+      };
+      break;
+    case "updateInputPassword":
+      state = {
+        ...state,
+        inputPassword: action.value,
+      };
+      break;
+    case "loggedInUser":
+      const foundUser = user.find((u) => u.email === state.inputEmail);
+      // console.log(foundUser);
+      if (foundUser) {
+        if (foundUser.password === foundUser.inputPassword) {
+          state = {
+            ...state,
+            loggedInUser: foundUser.email,
+          };
+        }
+      }
+      break;
+    default:
+      console.warn("unkwon Action");
+  }
+  return state;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          {state.loggedInUser ? (
+            <h1>welcome to our website {state.loggedInUser} </h1>
+          ) : (
+            <>
+              <div>
+                <input
+                  type="email"
+                  placeholder="email"
+                  value={state.inputEmail}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "updateInputEmail",
+                      value: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="email"
+                  value={state.inputPassword}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "updateInputPassword",
+                      value: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <div>
+                <button onClick={() => dispatch({ type: "loggedInUser" })}>
+                  Send
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </header>
     </div>
   );
